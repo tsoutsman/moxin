f2e0c06ca3e66b4c7cfa0f94967eb22fb4b0e837
Klim Tsoutsman <klim@tsoutsman.com>
2024-06-26 10:52:05 +1000

    Temp 2
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 use std::fs::File;
 
+const LOG_FILE: &str = "/Users/klim/Projects/robius/shared-directory-utm/moxin/log.txt";
+
 fn main() {
     println!("Moxin was invoked with args: {:#?}", std::env::args().collect::<Vec<_>>());
 
     use std::io::Write;
-    std::fs::write("/Users/klim/Projects/robius/shared-directory-utm/moxin/log", "a").unwrap();
-
     robius_url_handler::register_handler(handler);
-    let mut file = File::options().append(true).open("/Users/klim/Projects/robius/shared-directory-utm/moxin/log").unwrap();
-    write!(file, "c").unwrap();
     moxin::app::app_main()
 }
 
 fn handler(s: &str) {
     use std::io::Write;
-    let mut file = File::options().append(true).open("/Users/klim/Projects/robius/shared-directory-utm/moxin/log").unwrap();
-    write!(file, "\n").unwrap();
-    write!(file, "ad: {}", s).unwrap();
+    let mut file = File::options().append(true).open(LOG_FILE).unwrap();
+    write!(file, "handled: {}\n", s).unwrap();
 }
